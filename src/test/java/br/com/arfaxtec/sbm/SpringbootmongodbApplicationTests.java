b375f042eb3b8b67f02e983267888fcdb103bbf7
lheanndroaluno <leandroamorimpb@gmail.com>
2021-01-22 22:13:03 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/br/com/arfaxtec/sbm/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arfaxtec.sbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
